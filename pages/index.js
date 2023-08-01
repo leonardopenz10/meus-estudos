47f8556b54ec246943dfe0bcf0447504e0ced335
Leonardo Penz <59891933+leonardopenz10@users.noreply.github.com>
2023-08-01 14:31:03 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-function Home(){
-    return <h1>[Mensagem motivacional aqui]</h1>
+function Home() {
+  return <h1>[Mensagem motivacional aqui]</h1>;
 }
 
+function teste() {
+  console.log("Teste");
+}
 export default Home;
